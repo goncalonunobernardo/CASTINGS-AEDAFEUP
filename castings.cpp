eb821029f1b382a98c171d4569110c3ce30a2ab4
Ana Rita Norinho <ritanorinho98@hotmail.com>
2017-11-13 20:46:40 +0000

    Funções para Adicionar candidatos/jurados/sessoes. Excecao candidato repetida.

--- castings.cpp
+++ castings.cpp
@@ -17,6 +17,11 @@ void Pessoa::setMorada(string morada)
 {
 	this->morada = morada;
 }
+bool Pessoa::operator==(Pessoa & p1)
+{
+	
+	return false;
+}
 Pessoa::Pessoa(string nome, string morada, string genero) {
 	this->nome = nome;
 	this->morada = morada;
@@ -38,10 +43,25 @@ void Jurado::setTelemovel(string telemovel)
 	this->telemovel = telemovel;
 }
 
-
-vector<int> Sessao::getClassificacao() const
+bool Jurado::operator==(Jurado & j1)
 {
-	return classificacao;
+	if (this->getNome() == j1.getNome() && this->getMorada() == j1.getMorada() && this->getTelemovel()==j1.getTelemovel()) return true;
+	return true;
+
+	return false;
+}
+
+
+
+
+string Sessao::getGenero() const
+{
+	return genero;
+}
+
+string Sessao::getData() const
+{
+	return data;
 }
 
 string Sessao::getResponsavel() const
@@ -49,15 +69,28 @@ string Sessao::getResponsavel() const
 	return responsavel;
 }
 
-vector<Jurado> Sessao::getJurados_sessao()const 
+vector<Jurado> & Sessao::getJurados_sessao()const 
 {
 	return jurados_sessao;
 }
 
-vector<Candidato> Sessao::getConcorrentes_iniciais() const
+vector<Candidato> & Sessao::getConcorrentes_iniciais() const
 {
 	return concorrentes_iniciais;
 }
+
+int Sessao::getNumVagas() const
+{
+	int vagas = numMaxCandidatos - concorrentes_iniciais.size();
+	return vagas;
+}
+
+bool Sessao::operator==(Sessao & s1)
+{
+	if (s1.getGenero() == this->genero && this->data == s1.getData()) return true;
+	return false;
+}
+
 
 Candidato::Candidato(string nome, string morada, string genero, string data_nascimento):Pessoa(nome,morada,genero)
 {
@@ -74,14 +107,50 @@ vector<Sessao> Candidato::getSessoes() const
 	return sessoes;
 }
 
-vector<Jurado> Castings::getJurados() const
+bool Candidato::operator==(Candidato & c1)
 {
-	return jurados;
+	if (this->getNome() == c1.getNome() && this->getMorada() == c1.getMorada() && this->getDataNascimento()==c1.getDataNascimento()) return true;
+	return false;
 }
 
-vector<Candidato> Castings::getCandidatos() const
+
+
+bool Castings::adicionaCandidato(Candidato *c1)
 {
-	return candidatos;
+	for (size_t i = 0; i < candidatos.size(); i++) {
+			if (candidatos.at(i)==c1)
+			return false;
+		}
+	candidatos.push_back(c1);
+	return true;
+}
+
+bool Castings::adicionaJurado(Jurado *j1)
+{
+	for (size_t i = 0; i < jurados.size(); i++) {
+		if (jurados.at(i) == j1) return false;
+	}
+	jurados.push_back(j1);
+	return true;
+}
+
+bool Castings::adicionaCandidatoSessao(Candidato & c1, Sessao &s1)
+{
+
+	for (size_t i = 0; i < sessoes.size(); i++) {
+		if (s1 == sessoes.at(i)) {
+			if (sessoes.at(i).getNumVagas() > 0)
+			{
+				for (size_t j = 0; j < sessoes.at(i).getConcorrentes_iniciais().size(); j++) {
+					if (sessoes.at(i).getConcorrentes_iniciais().at(i) == c1) throw CandidatoRepetido(c1);
+				}
+				sessoes.at(i).getConcorrentes_iniciais().push_back(c1);
+				return true;
+			}
+			else
+				return false;
+		}
+	}
 }
 
 int Pontuacao::getId() const
--- castings.h
+++ castings.h
@@ -10,9 +10,9 @@ class Candidato;
 class Sessao;
 class Pontuacao;
 class Castings;
-
+class CandidatoRepetido;
 class Pessoa {
-private:
+protected:
 	string nome;
 	string morada;
 	string genero;
@@ -22,6 +22,7 @@ public:
 	string getMorada() const;
 	string getGenero() const;
 	void setMorada(string morada);
+	virtual bool operator==(Pessoa &p1);
 };
 
 class Jurado : Pessoa {
@@ -31,8 +32,21 @@ public:
 	Jurado(string nome, string morada, string genero, string telemovel);
 	string getTelemovel() const;
 	void setTelemovel(string telemovel);
+	bool operator==(Jurado &j1);
 	
 
+};
+class Candidato : Pessoa {
+private:
+	string data_nascimento;
+	static int numInscricao; //atribuido  automaticamente quando realizam a 1ªinscricao
+	vector<Sessao> sessoes;
+	vector<Pontuacao> pontuacoes;
+public:
+	Candidato(string nome, string morada, string genero, string data_nascimento);
+	string getDataNascimento() const;
+	vector<Sessao> getSessoes() const;
+	bool operator==(Candidato &c1);
 };
 class Pontuacao {
 private:
@@ -49,41 +63,51 @@ public:
 };
 class Sessao {
 private:
-	vector <int> classificacao;//Em que a primeira classificação é dada pelo primeiro jurado, a segunda pelo segundo e a terceira pelo terceiro
+	static int id;
 	int fase; //1 ou 2
 	int numMaxCandidatos; 
+	int numVagas;
 	string responsavel;
 	string genero;
 	string data; //A empresa não organiza mais que uma sessao do mesmo género por dia
-	vector<Jurado> jurados_sessao; //Cada sessao é composta por 3 jurados
-	vector <Candidato> concorrentes_iniciais; //vector composto por todos os candidatos à 1ªfase
-	vector <Candidato> concorrentes_finais; //concorrentes que passam à 2ªfase;
+	vector<Jurado> &jurados_sessao; //Cada sessao é composta por 3 jurados
+	vector <Candidato> &concorrentes_iniciais; //vector composto por todos os candidatos à 1ªfase
+	vector <Candidato> &concorrentes_finais; //concorrentes que passam à 2ªfase;
 public:
-	int getOrdem() const;
-	vector<int> getClassificacao() const;
+	string getGenero() const;
+	string getData() const;
 	string getResponsavel() const;
-	vector<Jurado> getJurados_sessao() const;
-	vector <Candidato> getConcorrentes_iniciais() const;
+	vector<Jurado> & getJurados_sessao() const;
+	vector <Candidato> & getConcorrentes_iniciais() const;
+	int getNumVagas() const;
+	bool operator==(Sessao &s1);
 };
-class Candidato : Pessoa {
-private:
-	string data_nascimento;
-	static int numInscricao; //atribuido  automaticamente quando realizam a 1ªinscricao
-	vector<Sessao> sessoes;
-	vector<Pontuacao> pontuacoes;
-public:
-	Candidato(string nome, string morada, string genero, string data_nascimento);
-	string getDataNascimento() const;
-	vector<Sessao> getSessoes() const;
-};
+
 
 
 class Castings {
 private:
-	vector<Jurado> jurados;
-	vector <Candidato> candidatos;
+	vector<Jurado*> jurados;
+	vector <Candidato*> candidatos;
+	vector<Sessao> sessoes;
 public:
 	Castings();
-	vector <Jurado> getJurados() const;
-	vector<Candidato> getCandidatos() const;
+	bool adicionaCandidato(Candidato *c1);
+	bool adicionaJurado(Jurado *j1);
+	bool adicionaCandidatoSessao(Candidato &c1, Sessao &s1);
 };
+class CandidatoRepetido {
+	Candidato &c1;
+public:
+	CandidatoRepetido(Candidato &c2) : c1(c2) {};
+	Candidato & getCandidato() const {
+		return c1;
+	}
+
+};
+ostream & operator<<(ostream &os, const CandidatoRepetido &c1) {
+	os << "CANDIDATO REPETIDO" << endl;
+	//os << "NOME: " << c1.getCandidato().getNome();
+	return os;
+
+}
