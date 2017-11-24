56ed4386b15434e18abbac0921ca23ccca039a2f
Gonçalo Bernardo <up201606058@fe.up.pt>
2017-11-24 23:13:15 +0000

    DOCUMENTATION DONE

--- menus.h
+++ menus.h
@@ -6,32 +6,89 @@
 #include <string>
 #include "castings.h"
 using namespace std;
+
+/**
+@brief function to display full logo of company
+*/
 void Menu();
+/**
+@brief function to display invalid inputs
+*/
 void InvalidInputMenu();
+/**
+@brief function to display our Main Menu
+*/
 void Menu_Principal();
+/**
+@brief function to display option on what to add to the auditions
+*/
 void Menu_Adicionar();
+/**
+@brief function to display option on what to remove from the auditions
+*/
 void Menu_Remover();
-/*
-void Menu_Alterar();*/
+/**
+@brief function to display option on informations to show
+*/
 void Menu_Informacoes();
+/**
+@brief function to display ALL genres available
+*/
 void Menu_Artes();
+/**
+@brief function to end program
+*/
 void Termina_Programa();
-
+/**
+@brief function to display option on what files to save
+*/
 void Menu_Gravar_Ficheiro();
+/**
+@brief function to save Candidates'file
+*/
 void Grava_Ficheiro_Candidatos();
+/**
+@brief function to save Juries'file
+*/
 void Grava_Ficheiro_Jurados();
+/**
+@brief function to save Sessions'file
+*/
 void Grava_Ficheiro_Sessoes();
-
-//void Menu_MostrarResultados();
-
+/**
+@brief object to create Candidates
+*/
 Candidato criar_Candidato();
+/**
+@brief object to create Juries
+*/
 Jurado criar_Jurado();
+/**
+@brief object to create Sessions
+*/
 Sessao criar_Sessao();
-string nome();
+/**
+@brief object to create Dates
+*/
 Data dataSessao();
+/**
+@brief object to create Sessions
+*/
 Sessao criarSessao();
+/**
+@brief function to show information of candidates on program just like if you were seeing in txt file
+*/
 void txt_candidatos();
+/**
+@brief function to show information of juries on program just like if you were seeing in txt file
+*/
 void txt_jurados();
+/**
+@brief function to show information of sessions on program just like if you were seeing in txt file
+*/
 void txt_sessoes();
+/**
+@brief function to show winners of the auditions
+*/
 void mostrarVencedores();
-void eliminarSessao();
+string nome();
